9722dbc07cdc7c6c00361152bb08041eec56246a
Jos Santa <joset.sistemas@gmail.com>
2022-08-31 16:57:37 -0500

    basicos-template-incio

---
+++ src/app/shared/sidemenu/sidemenu.component.ts
@@ -0,0 +1,48 @@
+import { Component,  } from '@angular/core';
+
+interface MenuItem //se crea para el mejos manejo de (objeto) rutas.
+{
+ texto:string,
+ ruta:string
+}
+
+@Component({
+  selector: 'app-sidemenu',
+  templateUrl: './sidemenu.component.html',
+  styles: [
+  ]
+})
+export class SidemenuComponent{
+
+//Creamos el arreglo para recorrer con *ngFor y mostrar las rutas.
+templateMenu: MenuItem[] = [
+  {
+    texto:'Básicos',
+    ruta:'./template/basicos'
+  },
+  {
+    texto:'Dinámicos',
+    ruta:'./template/dinamicos'
+  },
+  {
+    texto:'Switches',
+    ruta:'./template/switches'
+  }
+]
+
+reactiveMenu: MenuItem[] = [
+  {
+    texto:'Básicos',
+    ruta:'./reactive/basicos'
+  },
+  {
+    texto:'Dinámicos',
+    ruta:'./reactive/dinamicos'
+  },
+  {
+    texto:'Switches',
+    ruta:'./reactive/switches'
+  }
+]
+
+}
