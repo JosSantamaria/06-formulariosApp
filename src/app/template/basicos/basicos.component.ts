c0624283326e3d541bde4f4a349dfc8a4b26651f
Jos Santa <joset.sistemas@gmail.com>
2022-10-28 01:28:32 -0500

    fin-custom-directive

--- src/app/template/basicos/basicos.component.ts
+++ src/app/template/basicos/basicos.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
+import { Form, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-basicos',
@@ -9,13 +9,50 @@ import { NgForm } from '@angular/forms';
 })
 export class BasicosComponent implements OnInit {
 
+  //Nos permite poder ver elementos(hijos) dentro del componente padre(principal(basicos.component) )
+  @ViewChild('miFormulario') miFormulario!: NgForm;
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  /** Acceso a controles de formulario:
+   * es necesario acceder al arreglo
+   * de controls con el_nombre:string del
+   * elemento a controlar o usar.
+  */
 
-  guardar( miFormulario: NgForm ){
-    console.log(miFormulario.value);
+  //validacion de nombre
+  /**
+   * Retorna  ( true | false ) pregunta con '?'
+   * Si el formulario existe &
+   * Si el controls['producto']? existe &
+   * Si es ivalido.
+   */
+   nombreValido():boolean{
+    return this.miFormulario?.controls['producto']?.invalid
+      && this.miFormulario?.controls['producto']?.touched;
   }
+
+  /**
+   * Retorna  ( true | false ) pregunta con '?'
+   * Si el formulario existe &
+   * Si el controls['precio']? existe &
+   * Si es 0 o mayor.
+   */
+
+  precioValido():boolean{
+    return this.miFormulario?.controls['precio']?.touched
+      && this.miFormulario?.controls['precio']?.value < 0
+  }
+
+  // guardar( miFormulario: NgForm ){
+  guardar( ){
+    console.log(this.miFormulario);
+  }
+
+
+
+
 }
---
+++ src/app/template/directives/custom-min.directive.ts
@@ -0,0 +1,62 @@
+import { Directive, Input } from "@angular/core";
+import { FormControl, NG_VALIDATORS, ValidationErrors, Validator } from "@angular/forms";
+
+@Directive({
+  selector: '[customMin][ngModel]',
+  providers:[{
+    provide: NG_VALIDATORS,
+    useExisting: CustomMinDirective,
+    multi: true
+  }]
+})
+/**
+ * Objetivo: Crear una Directiva personalizada a traves del decorador @Directive
+ * Asignamos el selector o nombre de esta con [CustonMin], la cual se pasa como atributo al [ngModel].
+ * Obtenemos los modulos necesarios de NG_VALIDATORS, usamos la clase actual para asignarla a ala directiva nueva
+ * Class - CustonMinDirective()
+ *
+ * @usageNotes
+ * Directiva Personalizada extiende
+ * atributos de un elemento del DOM
+ * para poder manejarlos.
+ */
+
+export class CustomMinDirective implements Validator{
+
+/**
+ * Obtenemos una entradaa del componente padre (input)
+ * luego la establecemos como una de las propiedades
+ * para poder hacer la evaluacion.
+ * @Input
+ */
+  @Input() minimo!: number;
+
+
+  constructor(){}
+  /**
+   * Creamos una nueva instancia del @method Validate
+   * para poder usar sus propiedades, pasamos el parametro @param control
+   * e indicamos que es de tipo @type FormControl
+   */
+  validate( control: FormControl ){
+    /**
+     * Asignamos el valor recibido al @param control
+     */
+    const inputValue = control.value
+
+    /*inputValue:Obtiene el valor del control de el input
+    *this.minimo:[minimo]="parametro"
+    ***Se Pone en llaves cuadradas para angular lo controle y convierta a number;
+    */
+
+    return ( inputValue < this.minimo)?{'customMin':true}:null;
+    /**
+     * Retorna si es true, cuando el valor de entrada(inputValue:)
+     * sea menor al minimo especificado([minimo]="parametro")
+     * sino, retorna null.
+     */
+  }
+
+}
+
+
